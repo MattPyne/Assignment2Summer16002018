787a69cf7276e0df7798195caf6248f9e1eb94ab
Unknown <10820789@my.uvu.edu>
2018-06-15 13:24:38 -0600

    Behaviors and floatdata

---
+++ Assignment2/Assets/Scripts/HealthBarBehavior.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBarBehavior : MonoBehaviour
+{
+
+	public FloatData HealthLevel;
+	public Image HealthImage;
+
+
+	private void Start()
+	{
+		HealthImage.GetComponent<Image>().color = Color.green;
+	}
+
+	private void Update()
+	{
+		HealthImage.fillAmount = HealthLevel.Value;
+		if (HealthLevel.Value < 0.5)
+			HealthImage.GetComponent<Image>() = Color.yellow;
+
+		if (HealthImage.Value < 0.33)
+			HealthImage.GetComponent<Image>() = Color.red;
+
+	}
+}
