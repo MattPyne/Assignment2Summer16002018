289d4554143fa72cba1e31078b1f7bec75534f36
Unknown <10820789@my.uvu.edu>
2018-06-15 17:42:09 -0600

    Do Subtraction

---
+++ Assignment2/Assets/Scripts/DoSubrtaction.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoSubrtaction : ScriptableObject
+{
+
+    public int c;
+
+    private void OnEnable()
+    {
+        c = Run(10, 3);
+    }
+
+    public int Run(int a, int b)
+    {
+        return a -= b;
+    }
+}
