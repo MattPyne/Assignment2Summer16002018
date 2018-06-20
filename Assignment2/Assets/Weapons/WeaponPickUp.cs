ff7ab511e18c3ea380f5bfde7d5d7b4af3a5c84b
Unknown <10820789@my.uvu.edu>
2018-06-20 11:22:29 -0600

    Lists and Collections

---
+++ Assignment2/Assets/Weapons/WeaponPickUp.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponPickUp : MonoBehaviour
+{
+
+	public WeaponObjects Weapon;
+	public WeaponsCollection Collection;
+
+	private void OnTriggerEnter(Collider onj)
+	{
+		Collection.CollectWeapon(Weapon);
+	}
+}
---
+++ Assignment2/Assets/Weapons/WeaponsCollection.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+
+public class WeaponsCollection : ScriptableObject
+{
+
+
+	public List<WeaponObjects> Weapons;
+
+	public void CollectWeapon(WeaponObjects weapon)
+	{
+		Weapons.Add(weapon);
+
+		foreach (var item in Weapons)
+		{
+			Debug.Log(item);
+
+
+			if (item.name == "Sword")
+			{
+				Debug.Log("You have the best Weapon!!!");
+			}
+		}
+	}
+}
