d01d0534fb5f9d4262044e144031c9a0c86c931d
Unknown <10820789@my.uvu.edu>
2018-07-11 11:02:56 -0600

    AI Controller

---
+++ Assignment2/Assets/AI Folder/AIControll.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.AI;
+
+[CreateAssetMenu]
+public class AIControll : ScriptableObject
+{
+
+	public float Speed;
+	public float AngularsSpeed;
+
+	public void ConfigAI(NavMeshAgent agent)
+	{
+		agent.speed = Speed;
+		agent.angularSpeed = AngularsSpeed;
+	}
+	
+}
---
+++ Assignment2/Assets/AI Folder/AIMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent( typeof(NavMeshAgent))]
+public class AIMovement : MonoBehaviour
+{
+
+	private NavMeshAgent Agent;
+	public Transform Destination;
+	public AIControll Control;
+	
+	
+
+	// Use this for initialization
+	void Start ()
+	{
+		Agent = GetComponent<NavMeshAgent>();
+		Control.ConfigAI(Agent);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		Agent.destination = Destination.position;
+
+	}
+}
