5fc510fb5a048177ad3bdf0a8bf9909e4acc421b
Unknown <10820789@my.uvu.edu>
2018-07-31 16:53:48 -0600

    mouse click bounce

---
+++ Assignment2/Assets/Animation/BounceOnClick.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BounceOnClick : MonoBehaviour
+{
+    private Animator anims;
+
+    private void Start()
+    {
+        anims = GetComponent<Animator>();
+    }
+
+    private void OnMouseDown()
+    {
+        anims.SetTrigger("CanBounce");
+    }
+}
