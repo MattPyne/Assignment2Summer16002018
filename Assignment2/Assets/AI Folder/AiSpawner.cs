002dae16f85b01cf7183e05c8e921aeee3cc3d8f
Unknown <10820789@my.uvu.edu>
2018-07-30 19:26:22 -0600

    levels

--- Assignment2/Assets/AI Folder/AiSpawner.cs
+++ Assignment2/Assets/AI Folder/AiSpawner.cs
@@ -8,22 +8,26 @@ public class AiSpawner : MonoBehaviour
 
 	public GameObject Ai;
 	public Transform Destination;
+	public Level CurrentLevel;
+	private int aiCount;
 
 	// Use this for initialization
 	void Start ()
 	{
+		aiCount = CurrentLevel.AiCount;
 		StartCoroutine(StartSpawn());
 	}
 
 	IEnumerator StartSpawn()
 	{
-		while (true)
+		while (aiCount > 0)
 		{
 			GameObject newAI = Instantiate(Ai);
 			
 			newAI.GetComponent<AIMovement>().Destination = Destination;
-			
-			yield return new WaitForSeconds(2);
+
+			aiCount--;
+			yield return new WaitForSeconds(CurrentLevel.Time);
 		}
 	}
 }
---
+++ Assignment2/Assets/Scripts/Level.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+[CreateAssetMenu]
+public class Level : ScriptableObject
+{
+
+    public int AiCount = 1;
+    public float Time = 2;
+}
