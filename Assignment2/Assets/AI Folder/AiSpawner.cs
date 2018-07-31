afc0f4ceda9fb64b945a72c440c1c638947fdea3
Unknown <10820789@my.uvu.edu>
2018-07-30 18:58:12 -0600

    AI spawn

---
+++ Assignment2/Assets/AI Folder/AiSpawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AiSpawner : MonoBehaviour
+{
+
+	public GameObject Ai;
+	public Transform Destination;
+
+	// Use this for initialization
+	void Start ()
+	{
+		StartCoroutine(StartSpawn());
+	}
+
+	IEnumerator StartSpawn()
+	{
+		while (true)
+		{
+			GameObject newAI = Instantiate(Ai);
+			
+			newAI.GetComponent<AIMovement>().Destination = Destination;
+			
+			yield return new WaitForSeconds(2);
+		}
+	}
+}
