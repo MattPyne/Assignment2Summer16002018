7022311e8cff407a3d1be61de75cf7e73d03427c
Unknown <10820789@my.uvu.edu>
2018-08-06 18:33:18 -0600

    more code for platforms

--- Assignment2/Assets/GameManager.cs
+++ Assignment2/Assets/GameManager.cs
@@ -6,13 +6,20 @@ public class GameManager : MonoBehaviour
 {
 
 	private float size;
+
+	[SerializeField] 
+	GameObject Platform;
+
+	private Vector3 lastPos;
 	
-	[SerializeField]
-	
-	
-	
-	void Start () {
-		
+	void Start ()
+	{
+		size = Platform.transform.localScale.x;
+		lastPos = Platform.transform.position;
+		for (int x = 0; x < 25; x++)
+		{
+			SpawnZ();
+		}
 	}
 	
 	// Update is called once per frame
@@ -22,11 +29,15 @@ public class GameManager : MonoBehaviour
 
 	private void SpawnX()
 	{
-		
+		GameObject platform = Instantiate(Platform) as GameObject;
+		platform.transform.position = lastPos + new Vector3(size, 0f, 0f);
+		lastPos = platform.transform.position;
 	}
 
 	private void SpawnZ()
 	{
-		
+		GameObject platform = Instantiate(Platform) as GameObject;
+		platform.transform.position = lastPos + new Vector3(0f, 0f, size);
+		lastPos = platform.transform.position;
 	}
 }
