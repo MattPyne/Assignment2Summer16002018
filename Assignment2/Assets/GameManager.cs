4fdf59b3a56fe556f192593d3f29960e5121fb8e
Unknown <10820789@my.uvu.edu>
2018-08-06 18:12:41 -0600

    platform instantiation

---
+++ Assignment2/Assets/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+	private float size;
+	
+	[SerializeField]
+	
+	
+	
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void SpawnX()
+	{
+		
+	}
+
+	private void SpawnZ()
+	{
+		
+	}
+}
