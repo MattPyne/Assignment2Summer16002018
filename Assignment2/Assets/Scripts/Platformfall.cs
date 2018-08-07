8ef9ddbbbd6fbfa9edd076531de1f62bda0780d2
Unknown <10820789@my.uvu.edu>
2018-08-07 17:49:58 -0600

    Finally got the platforms to fall and not be messed up

--- Assignment2/Assets/Scripts/Platformfall.cs
+++ Assignment2/Assets/Scripts/Platformfall.cs
@@ -4,18 +4,25 @@ using UnityEngine;
 
 public class Platformfall : MonoBehaviour
 {
+	
+	
 	void OnTriggerEnter(Collider col)
 	{
+		Debug.Log("Collision Function Entered");
 		if (col.gameObject.tag == "Player")
 		{
-			Invoke("FallDown", 2f);
+			Debug.Log("Collision function Entered");
+			Invoke("FallDown", 1f);
 		}
+		
+		     
 	}
-
 	private void FallDown()
 	{
 		this.GetComponentInParent<Rigidbody>().isKinematic = false;
-		Destroy(this.transform.parent.gameObject, 2f);
+		
 	}
+
+	
 }
 		
